9e99e33f2d49c393d223c7c3dc5b972837a0ef04
zhima490 <zhima.youzi@gmail.com>
2025-01-01 00:19:50 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,22 @@
-window.addEventListener('scroll', function() {
-    const p1 = document.querySelector('.p1');
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 150) {
-        const blurValue = Math.min((scrollPosition - 150) / 100, 10);
-        p1.style.filter = `blur(${2}px)`;
-    } else {
-        p1.style.filter = 'blur(0px)';
-    }
-});
-
-let currentImageIndex = 0;
+// 輪播圖片功能
 const images = [
-    '/images/sesame.png',
-    '/images/br.png'
+    '/images/index_photo/br1.webp',
+    '/images/index_photo/br2.webp',
+    '/images/index_photo/br3.webp',
+    '/images/index_photo/br4.webp'
 ];
 
-const rotatingImage = document.getElementById('rotating-image');
+let currentIndex = 0;
+const slideshowImage = document.getElementById('slideshow-image');
 
 function changeImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    rotatingImage.style.opacity = 0; 
-
+    slideshowImage.style.opacity = '0';
+    
     setTimeout(() => {
-        rotatingImage.src = images[currentImageIndex]; 
-        rotatingImage.style.opacity = 1;
-    }, 500); 
+        currentIndex = (currentIndex + 1) % images.length;
+        slideshowImage.src = images[currentIndex];
+        slideshowImage.style.opacity = '1';
+    }, 500);
 }
 
-setInterval(changeImage, 5000);
-
-let touchStartX = 0;
-
-rotatingImage.addEventListener('touchstart', function(event) {
-    touchStartX = event.touches[0].clientX;
-});
-
-rotatingImage.addEventListener('touchend', function(event) {
-    const touchEndX = event.changedTouches[0].clientX; 
-    if (touchEndX < touchStartX) {
-        changeImage();
-    } else if (touchEndX > touchStartX) {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        rotatingImage.style.opacity = 0;
-
-        setTimeout(() => {
-            rotatingImage.src = images[currentImageIndex]; 
-            rotatingImage.style.opacity = 1; 
-        }, 500);
-    }
-});
-
-document.getElementById('prev-button').addEventListener('click', function() {
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
-    rotatingImage.src = images[currentIndex];
-});
-
-document.getElementById('next-button').addEventListener('click', function() {
-    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
-    rotatingImage.src = images[currentIndex];
-});
-
-function openInNewTab(url) {
-    window.open(url, '_blank');
-}
+setInterval(changeImage, 10000);
