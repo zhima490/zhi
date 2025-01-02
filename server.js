0aee2801e555ef1ada98e8e57817773c53add105
zhima490 <zhima.youzi@gmail.com>
2025-01-02 21:50:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,16 @@ const transporter = nodemailer.createTransport({
 
 const PORT = process.env.PORT || 3000;
 
+// CORS 設定
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' 
+        ? ['https://zhimayouzi.onrender.com']
+        : ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 const authenticateToken = (req, res, next) => {
     const accessToken = req.cookies.accessToken;
     
