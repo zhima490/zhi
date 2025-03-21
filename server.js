25211ba460b0c58803a9251f0cf9db0d9baf4526
zhima490 <zhima.youzi@gmail.com>
2025-03-21 19:32:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -343,12 +343,12 @@ app.use(express.static(__dirname));
 
 // 路由處理
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html', 'index.html')));
-// app.get('/form', (req, res) => res.sendFile(path.join(__dirname, 'html', 'form.html')));
+app.get('/form', (req, res) => res.sendFile(path.join(__dirname, 'html', 'comingsoon.html')));
 app.get('/menu', (req, res) => res.sendFile(path.join(__dirname, 'html', 'menu.html')));
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'html', 'contact.html')));  // 添加 contact 路由
 app.get('/questions', (req, res) => res.sendFile(path.join(__dirname, 'html', 'questions.html')));  // 添加 questions 路由
 app.get('/line', (req, res) => res.sendFile(path.join(__dirname, 'html', 'line.html')));
-app.get('/comingsoon', (req, res) => res.sendFile(path.join(__dirname, 'html', 'comingsoon.html')));
+// app.get('/comingsoon', (req, res) => res.sendFile(path.join(__dirname, 'html', 'comingsoon.html')));
 app.get(['/bsl', '/backstage-login'], (req, res) => {
     const accessToken = req.cookies.accessToken;
     if (accessToken) {
