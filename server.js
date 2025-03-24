ab7d14093ccb780dd0991c396c3c0b64fcbf7db4
zhima490 <zhima.youzi@gmail.com>
2025-03-25 01:14:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -203,13 +203,6 @@ const AuthLog = mongoose.model('AuthLog', {
     timestamp: Date
 });
 
-function authenticateUser(req, res, next) {
-    if (req.session.user) {
-        return next();
-    }
-    res.redirect('/backstage-login');
-}
-
 function generateToken(length = 8) {
     return crypto.randomBytes(length).toString('hex').slice(0, length);
 }
@@ -393,8 +386,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -403,21 +394,6 @@ app.use(express.static(path.join(__dirname, 'html')));
 app.use(express.static(__dirname));
 
 // 路由處理
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const adminUsername = process.env.ADMIN_USERNAME;
-    const adminPassword = process.env.ADMIN_PASSWORD;
-    const developerUsername = process.env.DEVELOPER_USERNAME;
-    const developerPassword = process.env.DEVELOPER_PASSWORD;
-
-    if ((username === adminUsername && password === adminPassword) ||
-        (username === developerUsername && password === developerPassword)) {
-        req.session.user = { username };
-        return res.sendStatus(200);
-    }
-    res.status(401).send('登入失敗');
-});
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html', 'index.html')));
 app.get('/form', (req, res) => res.sendFile(path.join(__dirname, 'html', 'form.html')));
@@ -434,21 +410,21 @@ app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'html', 'con
 app.get('/questions', (req, res) => res.sendFile(path.join(__dirname, 'html', 'questions.html')));  // 添加 questions 路由
 app.get('/line', (req, res) => res.sendFile(path.join(__dirname, 'html', 'line.html')));
 // app.get('/comingsoon', (req, res) => res.sendFile(path.join(__dirname, 'html', 'comingsoon.html')));
-
-
-//新後台測試
-
-app.get(['/bs-t', '/backstage-test'], authenticateUser, (req, res) => {
-    const role = req.session.user.username === process.env.ADMIN_USERNAME ? '管理者' : 
-                 req.session.user.username === process.env.DEVELOPER_USERNAME ? '開發者' : '普通用戶';
-    res.render('backstage-test', { role });
+app.get(['/bsl', '/backstage-login'], (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    if (accessToken) {
+        try {
+            jwt.verify(accessToken, process.env.JWT_SECRET);
+            return res.redirect('/bs');
+        } catch (err) {
+            // 處理錯誤
+        }
+    }
+    res.sendFile(path.join(__dirname, 'html', 'backstage-login.html'));
 });
 
-// app.get(['/bs', '/backstage'], authenticateToken, (req, res) => {
-//      res.sendFile(path.join(__dirname, 'html', 'backstage.html'));
-
-app.get(['/bsl', '/backstage-login'], (req, res) => {
-    res.sendFile(path.join(__dirname, 'html', 'backstage-login.html'));
+app.get(['/bs', '/backstage'], authenticateToken, (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'backstage.html'));
 });
 
 connectToDatabase();
